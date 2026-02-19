dc520fbb75ea3a39f593d1fd1f2a953666f0241a
João Almeida <jooaooalmeida@icloud.com>
2026-02-19 18:37:27 +0000

    refractor: swapped bun stdin reading with readline for better interactivity support

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import { setCommand } from "@commands/register/set";
 import { helpCommand } from "@commands/general/help";
 import { commandManager } from "@commands/manager";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readline.createInterface({ input, output, prompt: "(URsiM)> " });
 
 commandManager.register(helpCommand);
 commandManager.register(setCommand);
 
-console.write(
-  "Welcome to the URsiM interactive shell! Use the help command or press CTRL + C to exit.\n> ",
+console.log(
+  "Welcome to the URsiM interactive shell! Use the help command or press CTRL + C to exit.",
 );
 
-for await (const line of console) {
-  commandManager.execute(line.split(" "));
-  console.write("> ");
+rl.prompt();
+for await (const line of rl) {
+  commandManager.execute(line.trim().split(" "));
+  rl.prompt();
 }

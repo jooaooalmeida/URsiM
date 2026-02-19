62029dc33e036c740d0e536314af4d5536b4a923
João Almeida <jooaooalmeida@icloud.com>
2026-02-19 18:14:41 +0000

    refractor: added category support for command parsing

--- src/commands/manager.ts
+++ src/commands/manager.ts
@@ -1,18 +1,46 @@
-import type { Command } from "./command";
+import { type Command, CommandCategory } from "./command";
 import { z } from "zod";
 
-export class CommandManager {
-  commands = new Map<string, Command<z.ZodType<any>>>();
+class CommandManager {
+  commandMap = new Map<string, Map<string, Command<z.ZodType<any>>>>();
 
   register(command: Command<any>) {
-    this.commands.set(command.name, command);
+    const categoryName = CommandCategory[command.category].toLowerCase();
+    if (!this.commandMap.has(categoryName)) {
+      this.commandMap.set(categoryName, new Map());
+    }
+
+    this.commandMap.get(categoryName)!.set(command.name, command);
   }
 
-  execute(commandName: string, args: string[]) {
-    const command = this.commands.get(commandName);
+  execute(args: string[]) {
+    const argOne = args[0]?.toLowerCase();
+    let category: string;
+    let commandName: string;
+
+    if (!argOne) {
+      return console.log("Command not found. Please use the help command for available commands.");
+    }
+
+    // Populate category and commandName variables. The If block checks if the first argument was a category or not.
+    if (this.commandMap.has(argOne)) {
+      category = args.shift()!.toLowerCase();
+      commandName = args.shift()?.toLowerCase() ?? "";
+    } else {
+      category = CommandCategory[CommandCategory.GENERAL].toLowerCase();
+      commandName = args.shift()!.toLowerCase();
+    }
+
+    if (!commandName) {
+      return console.log(`Missing command name for category ${category}.`);
+    }
+
+    const categoryMap = this.commandMap.get(category);
+
+    const command = categoryMap!.get(commandName);
     if (!command) {
       return console.log(
-        "No command found. Please use the help command to check available commands.",
+        `Could not find command ${commandName}. Please use the help command for available commands.`,
       );
     }
 
@@ -24,3 +52,5 @@ export class CommandManager {
     command.onRun(result.data);
   }
 }
+
+export const commandManager = new CommandManager();
